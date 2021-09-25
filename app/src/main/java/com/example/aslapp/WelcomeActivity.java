d6ed3ab400140a54172ad63e96d354e8929d9347
Anthony <anthony.pena0898@gmail.com>
2021-09-25 03:17:46 -0400

    changes to login for new layout

--- app/src/main/java/com/example/aslapp/WelcomeActivity.java
+++ app/src/main/java/com/example/aslapp/WelcomeActivity.java
@@ -7,11 +7,15 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,11 +27,14 @@ public class WelcomeActivity extends AppCompatActivity {
     public static final String TAG = WelcomeActivity.class.getSimpleName();
     public static final int AUTHUI_REQUEST_CODE = 12;
     Button btLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Hide status bar
+        mAuth = FirebaseAuth.getInstance();
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -36,18 +43,67 @@ public class WelcomeActivity extends AppCompatActivity {
         btLogin = findViewById(R.id.btLogin);
         btLogin.setOnClickListener(v -> goLoginRegister());
 
-        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
-            Log.d(TAG, "User not null " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
-            Intent i = new Intent(this, HomeActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);
-            this.finish();
-        }else{
-            Log.d(TAG, "User is null");
+    }
+
+    public void onEmailSignup(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            goHome();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(WelcomeActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            // Maybe go back??
+
+                        }
+                    }
+                });
+    }
+
+    public void onSignIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goHome();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(WelcomeActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            goHome();
         }
     }
 
+    public void goHome(){
+        Intent i = new Intent(this, HomeActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+        this.finish();
+    }
+
+    // TODO: Delete after we implement our own layouts
     public void goLoginRegister(){
         List<AuthUI.IdpConfig> provider = Arrays.asList(
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
@@ -64,6 +120,7 @@ public class WelcomeActivity extends AppCompatActivity {
                 .build(), AUTHUI_REQUEST_CODE);
     }
 
+    // TODO: We might need to delete this too
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
