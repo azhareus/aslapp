a39d97b22e9795c6b15e9ac3d26f8cf718a6fc6c
azhareus <azhar.hj@gmail.com>
2021-09-25 03:48:26 -0400

    opencv basics, might crash or it might not, we'll see

--- app/src/main/java/com/example/aslapp/MainActivity.java
+++ app/src/main/java/com/example/aslapp/MainActivity.java
@@ -1,59 +1,296 @@
 package com.example.aslapp;
 
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.provider.MediaStore;
+//import android.view.MenuItem;
+//import android.view.View;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentManager;
+//
+//import com.example.aslapp.fragments.CameraFragment;
+//import com.example.aslapp.fragments.HomeFragment;
+//import com.example.aslapp.fragments.ProfileFragment;
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//
+//import java.io.File;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+//    public String photoFileName = "photo.jpg";
+//    File photoFile;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        final FragmentManager fragmentManager = getSupportFragmentManager();
+//
+//        // define your fragments here
+//        final Fragment fragment1 = new HomeFragment();
+//        final Fragment fragment2 = new CameraFragment();
+//        final Fragment fragment3 = new ProfileFragment();
+//
+//        BottomNavigationView bottomNavigationView =
+//                (BottomNavigationView) findViewById(R.id.bottom_navigation);
+//
+//        // handle navigation selection
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        Fragment fragment;
+//                        switch (item.getItemId()) {
+//                            case R.id.action_home:
+//                                fragment = fragment1;
+//                                break;
+//                            case R.id.action_camera:
+//                                fragment = fragment2;
+//                                break;
+//                            case R.id.action_profile:
+//                            default:
+//                                fragment = fragment3;
+//                                break;
+//                        }
+//                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment)
+//                        .commit();
+//                        return true;
+//                    }
+//                });
+//        // Set default selection
+//        bottomNavigationView.setSelectedItemId(R.id.action_home);
+//
+//    }
+//
+//    public void onLaunchCamera(View view) {
+//        // create Intent to take a picture and return control to the calling application
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+////        // Create a File reference for future access
+////        photoFile = getPhotoFileUri(photoFileName);
+////
+////        // wrap File object into a content provider
+////        // required for API >= 24
+////        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files
+////        // -with-api-24-or-higher
+////        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath" +
+////                        ".fileprovider",
+////                photoFile);
+////        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+//
+//        // If you call startActivityForResult() using an intent that no app can handle, your app
+//        // will crash.
+//        // So as long as the result is not null, it's safe to use the intent.
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            // Start the image capture intent to take photo
+//            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+//        }
+//    }
+//
+//
+//}
+
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 
-import com.example.aslapp.fragments.CameraFragment;
-import com.example.aslapp.fragments.HomeFragment;
-import com.example.aslapp.fragments.ProfileFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
+
+    private boolean mIsColorSelected = false;
+    private Mat mRgba;
+    private Scalar mBlobColorRgba;
+    private Scalar mBlobColorHsv;
+    private ColorBlobDetector mDetector;
+    private Mat mSpectrum;
+    private Size SPECTRUM_SIZE;
+    private Scalar CONTOUR_COLOR;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(MainActivity.this);
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.color_blob_detection_activity_surface_view);
+
+        mOpenCvCameraView =
+                (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
 
-        final FragmentManager fragmentManager = getSupportFragmentManager();
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for " +
+                    "initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 
-        // define your fragments here
-        final Fragment fragment1 = new HomeFragment();
-        final Fragment fragment2 = new CameraFragment();
-        final Fragment fragment3 = new ProfileFragment();
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
 
-        BottomNavigationView bottomNavigationView =
-                (BottomNavigationView) findViewById(R.id.bottom_navigation);
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mDetector = new ColorBlobDetector();
+        mSpectrum = new Mat();
+        mBlobColorRgba = new Scalar(255);
+        mBlobColorHsv = new Scalar(255);
+        SPECTRUM_SIZE = new Size(200, 64);
+        CONTOUR_COLOR = new Scalar(255, 0, 0, 255);
+    }
 
-        // handle navigation selection
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment fragment;
-                        switch (item.getItemId()) {
-                            case R.id.action_home:
-                                fragment = fragment1;
-                                break;
-                            case R.id.action_camera:
-                                fragment = fragment2;
-                                break;
-                            case R.id.action_profile:
-                            default:
-                                fragment = fragment3;
-                                break;
-                        }
-                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
-                        return true;
-                    }
-                });
-        // Set default selection
-        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
 
+    public boolean onTouch(View v, MotionEvent event) {
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
+        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
+
+        int x = (int) event.getX() - xOffset;
+        int y = (int) event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x > 4) ? x - 4 : 0;
+        touchedRect.y = (y > 4) ? y - 4 : 0;
+
+        touchedRect.width = (x + 4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y + 4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width * touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE, 0, 0,
+                Imgproc.INTER_LINEAR_EXACT);
+
+        mIsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        if (mIsColorSelected) {
+            mDetector.process(mRgba);
+            List<MatOfPoint> contours = mDetector.getContours();
+            Log.e(TAG, "Contours count: " + contours.size());
+            Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(mBlobColorRgba);
+
+            Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+            mSpectrum.copyTo(spectrumLabel);
+        }
+
+        return mRgba;
+    }
+
+    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
     }
 }
