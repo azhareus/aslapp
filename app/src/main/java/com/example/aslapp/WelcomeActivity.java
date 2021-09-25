39d0376f96018156c0d22835e552779fc8055b91
Aldo Socarras <air.socarras@gmail.com>
2021-09-25 10:55:41 -0400

    Updated LoginActivity UI and refactored main activity

--- app/src/main/java/com/example/aslapp/WelcomeActivity.java
+++ app/src/main/java/com/example/aslapp/WelcomeActivity.java
@@ -2,156 +2,40 @@ package com.example.aslapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
+import com.example.aslapp.databinding.ActivityMainBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     public static final String TAG = WelcomeActivity.class.getSimpleName();
-    public static final int AUTHUI_REQUEST_CODE = 12;
-    Button btLogin;
-    private FirebaseAuth mAuth;
+
+    // Declare binding variable
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Hide status bar
-        mAuth = FirebaseAuth.getInstance();
+        // Set up instance of binding class
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        // Remove app bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
-
-        btLogin = findViewById(R.id.btLogin);
-        btLogin.setOnClickListener(v -> goLoginRegister());
-
-    }
-
-    public void onEmailSignup(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            goHome();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(WelcomeActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                            // Maybe go back??
-
-                        }
-                    }
-                });
-    }
-
-    public void onSignIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            goHome();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(WelcomeActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            goHome();
-        }
-    }
-
-    public void goHome(){
-        Intent i = new Intent(this, HomeActivity.class);
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
-        this.finish();
-    }
-
-    // TODO: Delete after we implement our own layouts
-    public void goLoginRegister(){
-        List<AuthUI.IdpConfig> provider = Arrays.asList(
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                //new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build()
-        );
-
-        startActivityForResult(AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(provider)
-                .setIsSmartLockEnabled(false)
-                .setTheme(R.style.GreenTheme)
-                .build(), AUTHUI_REQUEST_CODE);
-    }
-
-    // TODO: We might need to delete this too
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == AUTHUI_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-                // Successfully created a new user
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Log.d(TAG, "OnActivityResult: " + user.getEmail());
-                // Check if user is existing or new
-                if(user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()){
-                    // Toast.makeText(this, "Thank you for signing up!", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(this, "Thank you for signing up!", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(this, "Welcome back!", Toast.LENGTH_SHORT).show();
-                }
-                // Go to main
-                Intent i = new Intent(this, HomeActivity.class);
+        // btLogin.setOnClickListener(v -> goLoginRegister());
+        binding.btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(WelcomeActivity.this, LoginActivity.class);
                 startActivity(i);
-                this.finish();
             }
-            else {
-                Log.d(TAG, "Unable to login: ");
-                IdpResponse response = IdpResponse.fromResultIntent(data);
-                if(response == null){
-                    Log.d(TAG, "Cancelled sign in request");
-                }
-                else{
-                    Log.e(TAG, "Error: " + response.getError().getErrorCode());
-                }
-            }
-        }
+        });
+
     }
 }
