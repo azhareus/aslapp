bb7c97b088db5a3ef92e0beecb6d7181fb08a6cb
Anthony <anthony.pena0898@gmail.com>
2021-09-25 19:22:00 -0400

    Adds questions

--- app/src/main/java/com/example/aslapp/fragments/LearnFragment.java
+++ app/src/main/java/com/example/aslapp/fragments/LearnFragment.java
@@ -1,23 +1,42 @@
 package com.example.aslapp.fragments;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.aslapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LearnFragment extends Fragment {
 
+    int question = 0;
     Context context;
     Button btSubmit;
     HomeFragment homeFragment;
+    List<List<String>> questions;
+    ProgressBar progressBar;
+    ImageView ivQuestion;
+    RadioGroup group;
+    RadioButton q1;
+    RadioButton q2;
+    RadioButton q3;
+    RadioButton q4;
 
     public LearnFragment() {
         // Required empty public constructor
@@ -46,12 +65,81 @@ public class LearnFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        questions = new ArrayList<>();
+        createQuestions();
+        group = view.findViewById(R.id.radioQuestions);
+        q1 = view.findViewById(R.id.r1);
+        q2 = view.findViewById(R.id.r2);
+        q3 = view.findViewById(R.id.r3);
+        q4 = view.findViewById(R.id.r4);
+        ivQuestion = view.findViewById(R.id.ivQuestion);
+        progressBar = view.findViewById(R.id.progressBar2);
+
         btSubmit = view.findViewById(R.id.btSubmit);
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager().popBackStack();
+                if(question == 1) {
+                    if(group.getCheckedRadioButtonId() == R.id.r2){
+                        Toast.makeText(getActivity(), "Correct!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getActivity(), "Incorrect!", Toast.LENGTH_SHORT).show();
+                    }
+                    question = 0;
+                    getActivity().getSupportFragmentManager().popBackStack();
+                }else if(question < 1){
+                    if(group.getCheckedRadioButtonId() == R.id.r1){
+                        Toast.makeText(getActivity(), "Correct!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getActivity(), "Incorrect!", Toast.LENGTH_SHORT).show();
+                    }
+                    changeQuestion(question);
+                    question++;
+                }else{
+                    question = 0;
+                    changeQuestion(question);
+                }
             }
         });
+    }
+
+    private void createQuestions() {
+        List<String> options = new ArrayList<>();
+        options.add("Who");
+        options.add("Why");
+        options.add("When");
+        options.add("How");
+        questions.add(options);
+
+        options.clear();
+        options.add("Them");
+        options.add("When");
+        options.add("No");
+        options.add("Sure");
+        questions.add(options);
+    }
+
+    public void changeQuestion(int questionNumber){
+        List<String> currentQuestion = questions.get(questionNumber);
+
+        if(questionNumber == 0){
+            Drawable image = getResources().getDrawable(R.drawable.when);
+            Glide.with(context)
+                    .load(image)
+                    .into(ivQuestion);
+        }else{
+            Drawable image = getResources().getDrawable(R.drawable.where);
+            Glide.with(context)
+                    .load(image)
+                    .into(ivQuestion);
+        }
+
+        group.clearCheck();
+        q1.setText(currentQuestion.get(0));
+        q2.setText(currentQuestion.get(1));
+        q3.setText(currentQuestion.get(2));
+        q4.setText(currentQuestion.get(3));
+
+
     }
 }
