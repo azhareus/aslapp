0bd54a7c1fb121143f346de7ef992be0bb0e13e2
azhareus <azhar.hj@gmail.com>
2021-09-25 00:56:11 -0400

    set up basic bottom navigation view

--- app/src/main/java/com/example/aslapp/MainActivity.java
+++ app/src/main/java/com/example/aslapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.aslapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.aslapp.fragments.CameraFragment;
+import com.example.aslapp.fragments.HomeFragment;
+import com.example.aslapp.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment fragment1 = new HomeFragment();
+        final Fragment fragment2 = new CameraFragment();
+        final Fragment fragment3 = new ProfileFragment();
+
+        BottomNavigationView bottomNavigationView =
+                (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.action_home:
+                                fragment = fragment1;
+                                break;
+                            case R.id.action_camera:
+                                fragment = fragment2;
+                                break;
+                            case R.id.action_profile:
+                            default:
+                                fragment = fragment3;
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+
     }
 }
