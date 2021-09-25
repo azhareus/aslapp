60b04965e09e52f5997fb2fb7550c6dca92614d5
Anthony <anthony.pena0898@gmail.com>
2021-09-25 00:35:21 -0400

    Fixes login nav

--- app/src/main/java/com/example/aslapp/LoginActivity.java
+++ app/src/main/java/com/example/aslapp/LoginActivity.java
@@ -40,7 +40,6 @@ public class LoginActivity extends AppCompatActivity {
             this.finish();
         }else{
             Log.d(TAG, "User is null");
-            goLoginRegister();
         }
     }
 
